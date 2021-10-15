33e253e8b1379d09663ddaaf1618f8de742dabbe
Dani Lerner <dlerner97@gmail.com>
2021-10-15 17:00:04 -0400

    Fixed structure to allow compilation

--- app/HumanDetector.cpp
+++ app/HumanDetector.cpp
@@ -2,8 +2,9 @@
 #include <memory>
 #include <opencv2/opencv.hpp>
 
+#include "../include/Detection.hpp"
 #include "../include/HumanDetector.hpp"
 
-std::shared_ptr<cv::Mat> HumanDetector::prep_frame(cv::Mat& orig_frame) {}
+std::shared_ptr<cv::Mat> HumanDetector::prep_frame(cv::Mat& /* orig_frame */) {return std::shared_ptr<cv::Mat>{};}
 
-std::vector<Detection> HumanDetector::detect(cv::Mat& prepped_frame) {}
+std::vector<Detection> HumanDetector::detect(cv::Mat& /* prepped_frame */) {return std::vector<Detection>{};}
