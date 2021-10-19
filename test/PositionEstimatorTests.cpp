6da6b744c3e2a75588e3d380ef069b28eca17cce
Dani Lerner <dlerner97@gmail.com>
2021-10-19 14:06:49 -0400

    Added line at end of file

--- test/PositionEstimatorTests.cpp
+++ test/PositionEstimatorTests.cpp
@@ -44,7 +44,7 @@ TEST(PositionEstimatorTests, ApproximateZTest) {
   std::vector<Var> params{{"DX_CAM2ROBOT_CENTER", "m"}, {"DY_CAM2ROBOT_CENTER", "m"},
                           {"DZ_CAM2ROBOT_CENTER", "m"}, {"PITCH_CAM2ROBOT_CENTER", "rad"},
                           {"CAM_FOCAL_LEN", "m"}, {"CAM_PIXEL_DENSITY", "ppm"},
-                          {"AVG_HUMAN_HEIGHT", "m"}, {"DETECTION_PROBABILITY_THRESHOLD", "fraction"}, 
+                          {"AVG_HUMAN_HEIGHT", "m"}, {"DETECTION_PROBABILITY_THRESHOLD", "fraction"},
                           {"IMG_WIDTH_REQ", "px"}, {"IMG_HEIGHT_REQ", "px"}};
 
   ParamParser parser(params);
