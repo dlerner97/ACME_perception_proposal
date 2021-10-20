09f55f5dbe9d29783e644babc5aa2c2c3ffcbf05
Dani Lerner <dlerner97@gmail.com>
2021-10-19 21:57:18 -0400

    Added tParamParser testing suite as well as texfiles used in these tests.

---
+++ test/ParamParserTests.cpp
@@ -0,0 +1,89 @@
+#include <gtest/gtest.h>
+#include <fstream>
+
+#include "../include/ParamParser.hpp"
+
+TEST(ParamParserTest, TestTextFileExistence) {
+    std::vector<Var> params{};
+    ParamParser parser(params);
+    EXPECT_ANY_THROW(parser.parse_robot_params("../test/robot_params_textfiles/TextFileDoesNotExist.txt"));
+    EXPECT_NO_THROW(parser.parse_robot_params("../test/robot_params_textfiles/TextFileDoesExist.txt"));
+}
+
+TEST(ParamParserTest, VarVectorParserMismatchTest) {
+    std::vector<Var> params{{"test", "m"}};
+    ParamParser parser(params);
+    EXPECT_ANY_THROW(parser.parse_robot_params("../test/robot_params_textfiles/VarVectorMismatch.txt"));
+    EXPECT_NO_THROW(parser.parse_robot_params("../test/robot_params_textfiles/VarVectorMatch.txt"));
+}
+
+TEST(ParamParserTest, UnitTest) {
+    typedef std::vector<Var> vec;
+    vec params{{"no_unit_m", "m"}, {"m", "m"}, {"cm", "m"}, {"mm", "m"},
+               {"no_unit_rad", "rad"}, {"rad", "rad"}, {"radians", "rad"}, {"deg", "rad"}, {"degrees", "rad"},
+               {"no_unit_frac", "frac"}, {"fraction", "frac"}, {"frac", "frac"}, {"percent_sign", "frac"}, {"percent", "frac"},
+               {"no_unit_px", "px"}, {"px", "px"}, {"pixels", "px"},
+               {"no_unit_ppm", "ppm"}, {"ppm", "ppm"}, {"ppi", "ppm"}, {"ppmm", "ppm"}};
+
+    ParamParser parser(params);
+    auto result = parser.parse_robot_params("../test/robot_params_textfiles/UnitEq.txt");
+    
+    for (vec::size_type i = 0; i < 4; i++)
+        EXPECT_NEAR(result.at(params[0].name), result.at(params[i].name), 0.1);
+
+    for (vec::size_type i = 4; i < 9; i++)
+        EXPECT_NEAR(result.at(params[4].name), result.at(params[i].name), 0.1);
+
+    for (vec::size_type i = 9; i < 14; i++)
+        EXPECT_NEAR(result.at(params[9].name), result.at(params[i].name), 0.1);
+
+    for (vec::size_type i = 14; i < 17; i++)
+        EXPECT_NEAR(result.at(params[14].name), result.at(params[i].name), 0.1);
+
+    for (vec::size_type i = 17; i < 21; i++)
+        EXPECT_NEAR(result.at(params[17].name), result.at(params[i].name), 0.1);
+}
+
+TEST(ParamParserTest, UnitNotAllowedTest) {
+    typedef std::vector<Var> vec;
+    vec params{{"a", "m"}, {"b", "rad"}, {"c", "frac"}, {"d", "px"}, {"e", "ppm"}};
+    ParamParser parser(params);
+
+    std::ofstream ofs;
+    std::string path = "../test/robot_params_textfiles/UnitNotAllowed.txt";
+    std::vector<std::string> vector{"a = 1 [px]", "b = 1 [e]", "c = 1 [m]", "d = 1 [ppm]", "e = 1 [%]"};
+    for (const auto& str : vector) {
+        ofs.open(path, std::ofstream::out | std::ofstream::trunc);
+        ofs << str << std::endl;
+        ofs.close();
+
+        ASSERT_ANY_THROW(parser.parse_robot_params(path));
+    }
+}
+
+TEST(ParamParserTest, VariousInputsTest) {
+    typedef std::vector<Var> vec;
+    vec params{{"a", "m"}};
+    ParamParser parser(params);
+
+    std::ofstream ofs;
+    std::string path = "../test/robot_params_textfiles/VariousInvalidInputs.txt";
+    std::vector<std::string> vector{"a = 1 m]", "a = 1 [m", "a = 1 m", "1 = a [m]", "a = h [m]", "a = 1 = 1 [m]"};
+    for (const auto& str : vector) {
+        ofs.open(path, std::ofstream::out | std::ofstream::trunc);
+        ofs << str << std::endl;
+        ofs.close();
+
+        ASSERT_ANY_THROW(parser.parse_robot_params(path));
+    }
+
+    vector = {"a = 1 [m]", "a = 1[m]", "a =1[m]", "a=1[m]", "    a =     1    [m]", "a = 1", "This is a comment\na = 1"};
+    for (const auto& str : vector) {
+        ofs.open(path, std::ofstream::out | std::ofstream::trunc);
+        ofs << str << std::endl;
+        ofs.close();
+
+        ASSERT_NO_THROW(parser.parse_robot_params(path));
+        EXPECT_EQ(parser.parse_robot_params(path).at("a"), 1);
+    }
+}
