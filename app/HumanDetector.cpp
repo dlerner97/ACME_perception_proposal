35365f49ad4ff3c8b48c8a4aa81deb62b153c46a
dngo13 <diane.ngo1321@gmail.com>
2021-10-22 20:32:34 -0400

    Updated HumanDetector with get function for image dimensions

--- app/HumanDetector.cpp
+++ app/HumanDetector.cpp
@@ -10,3 +10,7 @@ std::shared_ptr<cv::Mat> HumanDetector::prep_frame(cv::Mat& /* orig_frame */) {
 }
 
 std::vector<Detection> HumanDetector::detect(cv::Mat& /* prepped_frame */) {return std::vector<Detection>{};}
+
+std::array<int, 2> HumanDetector::get_img_dims() {
+    return img_dim_;
+}
--- include/HumanDetector.hpp
+++ include/HumanDetector.hpp
@@ -3,11 +3,21 @@
 #include <array>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include <memory>
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
 
 #include "Detection.hpp"
+
+using namespace cv;
+using namespace dnn;
 
 class HumanDetector {
   private:
@@ -16,7 +26,7 @@ class HumanDetector {
      * 
      * @details width, height
      */
-    std::array<int, 2> img_dim{};
+    std::array<int, 2> img_dim_{};
     std::vector<std::string> detection_classes{};
 
     // void populate_detection_classes(std::string coco_name_path);
@@ -24,8 +34,9 @@ class HumanDetector {
   public:
     HumanDetector(const std::unordered_map<std::string, double>& robot_params, const std::string& coco_name_path,
                   const std::string& yolo_cfg_path, const std::string& yolo_weight_path) {
-      img_dim[0] = robot_params.at("IMG_WIDTH_REQ");
-      img_dim[1] = robot_params.at("IMG_HEIGHT_REQ");
+      img_dim_[0] = robot_params.at("IMG_WIDTH_REQ");
+      img_dim_[1] = robot_params.at("IMG_HEIGHT_REQ");
+//      get_img_dims(img_dim_);
 
       // detection_classes = populate_detection_classes();
 
@@ -47,5 +58,18 @@ class HumanDetector {
      * @return A detection obj for each human detected in frame.
      */
     std::vector<Detection> detect(cv::Mat&);
+
+    /**
+     * @brief Gets the image dimensions (width and height)
+     * @return Array of image dimensions
+     */
+    std::array<int, 2> get_img_dims();
+
+    // Give the configuration and weight files for the model
+    String modelConfiguration = "../robot_params/yolov4.cfg";
+    String modelWeights = "../robot_params/yolov4.weights";
+
+    // Load the network
+    Net net = readNetFromDarknet(modelConfiguration, modelWeights);
 };
 
