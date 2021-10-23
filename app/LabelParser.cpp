364933834cd25f043f1f9dfbebfe647cdd77ade1
Dani Lerner <dlerner97@gmail.com>
2021-10-23 15:26:38 -0400

    Created LabelParser class for HumanDetection testing

--- app/LabelParser.cpp
+++ app/LabelParser.cpp
@@ -1,27 +1,94 @@
 #include <vector>
 #include <string>
+#include <memory>
 #include <filesystem>
+#include <fstream>
 #include <iostream>
 #include <algorithm>
 #include <stdexcept>
 #include <opencv2/opencv.hpp>
 
+#include "../include/utils.hpp"
+#include "../include/Detection.hpp"
 #include "../include/LabelParser.hpp"
 
-std::string cutdown_name(std::string name) {
-    auto first_slash = find(name.begin(), name.end(), '/');
-    if (first_slash != name.end()) {
-        return cutdown_name(std::string(first_slash+1, name.end()));
-    }
-    auto dot = find(name.begin(), name.end(), '.');
-    if (dot == name.end())
-        throw std::invalid_argument("Cannot find file type.");
+std::array<int, 2> LabelParser::parse_corner(const std::string& corner) {
+    auto open_bracket = std::find(corner.begin(), corner.end(), '[');
+    if (open_bracket == corner.end())
+        throw InvalidFile();
 
-    return std::string(name.begin(), dot);
+    auto close_bracket = std::find(open_bracket, corner.end(), ']');
+    if (close_bracket == corner.end())
+        throw InvalidFile();
+
+    auto corner_vals = split(std::string(open_bracket+1, close_bracket), ',');
+    if (corner_vals.size() != 2)
+        throw InvalidFile();;
+
+    std::array<int, 2> out{};
+    for (std::size_t i=0; i < 2; i++) {
+        try {
+            out[i] = std::stoi(corner_vals[i]);
+        } catch (std::invalid_argument const&) {
+            throw InvalidFile();
+        }
+    }
+    return out;
 }
 
-std::vector<TestImage> LabelParser::read_labeled_test_images(const std::string& dir_path) {
-    for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
-        std::cout << cutdown_name(entry.path()) << std::endl;
+Detection LabelParser::parse_detection(const std::string& corner1, const std::string& corner2) {
+    Detection detection;
+    std::array<int, 2> corner_coords1;
+    std::array<int, 2> corner_coords2;
+
+    try {
+        corner_coords1 = parse_corner(corner1);
+        corner_coords2 = parse_corner(corner2);
+    } catch (InvalidFile const&) {
+        throw InvalidFile();
     }
+
+    detection.x = std::min(corner_coords1[0], corner_coords2[0]);
+    detection.y = std::min(corner_coords1[1], corner_coords2[1]);
+    detection.width = std::max(corner_coords1[0], corner_coords2[0]) - detection.x;
+    detection.height = std::max(corner_coords1[1], corner_coords2[1]) - detection.y;
+    return detection;
+}
+
+std::shared_ptr<TestImage> LabelParser::parse_file(const std::string& file_path) {
+    std::ifstream infile(file_path.c_str());
+    std::shared_ptr<TestImage> test_image_ptr = std::make_shared<TestImage>();
+    if (infile) {
+        std::string file_name;
+        getline(infile, file_name);
+
+        std::string line;
+        while (getline(infile, line)) {
+            if (line == "Detection") {
+                std::string top_left, bottom_right;
+                getline(infile, top_left);
+                getline(infile, bottom_right);
+                try {
+                    auto detection = parse_detection(top_left, bottom_right);
+                    test_image_ptr->all_detections.push_back(detection);
+                } catch (InvalidFile const&) {
+                    throw InvalidFile("File " + file_name + " is invalid. Please fix file before continuing.");
+                }
+            }
+        }
+        test_image_ptr->name = file_name;
+        test_image_ptr->img = cv::imread("../dataset/1/" + file_name + ".png");
+        return test_image_ptr;
+    }
+    throw InvalidFile("Path " + file_path + " is invalid. Please add the file before continuing.");      
+}
+
+std::vector<std::shared_ptr<TestImage> > LabelParser::read_labeled_test_images(const std::string& dir_path) {
+    std::vector<std::shared_ptr<TestImage> > out{};
+    for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
+            auto test_image_ptr = parse_file(entry.path());
+            out.push_back(test_image_ptr);
+    }
+
+    return out;
 }
--- include/LabelParser.hpp
+++ include/LabelParser.hpp
@@ -1,19 +1,25 @@
 #pragma once
 
+#include <array>
 #include <vector>
 #include <string>
+#include <memory>
 #include <opencv2/opencv.hpp>
 
 #include "./Detection.hpp"
 
 struct TestImage {
+    std::string name;
     std::vector<Detection> all_detections{};
     cv::Mat img;
 };
 
 class LabelParser {
-//   private:
+  private:
+    std::array<int, 2> parse_corner(const std::string& corner);
+    Detection parse_detection(const std::string& corner1, const std::string& corner2);
 
   public:
-    std::vector<TestImage> read_labeled_test_images(const std::string& dir_path);
+    std::shared_ptr<TestImage> parse_file(const std::string& file_path); 
+    std::vector<std::shared_ptr<TestImage> > read_labeled_test_images(const std::string& dir_path);
 };
