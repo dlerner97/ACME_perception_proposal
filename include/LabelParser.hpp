d5f0ee3e96fffa8b504ab36ca055a3ea65ba9d12
Dani Lerner <dlerner97@gmail.com>
2021-10-24 16:44:50 -0400

    Added Doxygen comments

--- include/LabelParser.hpp
+++ include/LabelParser.hpp
@@ -16,10 +16,37 @@ struct TestImage {
 
 class LabelParser {
   private:
+    /**
+     * @brief Parses an individual corner. Conversion from string form to int
+     * 
+     * @param corner 
+     * @return std::array<int, 2> corner x-coord, y-coord
+     */
     std::array<int, 2> parse_corner(const std::string& corner);
+
+    /**
+     * @brief Parses an individual detection
+     * 
+     * @param corner1: std::string
+     * @param corner2: std::string
+     * @return Detection instance
+     */
     Detection parse_detection(const std::string& corner1, const std::string& corner2);
 
   public:
+    /**
+     * @brief Parses labeled image file
+     * 
+     * @param file_path: std::string
+     * @return std::shared_ptr<TestImage> shared pointer to a TestImage instance. 
+     */
     std::shared_ptr<TestImage> parse_file(const std::string& file_path); 
+
+    /**
+     * @brief Parses ever labeled image file in the input directory
+     * 
+     * @param dir_path: path of the desired dir
+     * @return std::vector<std::shared_ptr<TestImage> >
+     */
     std::vector<std::shared_ptr<TestImage> > read_labeled_test_images(const std::string& dir_path);
 };
