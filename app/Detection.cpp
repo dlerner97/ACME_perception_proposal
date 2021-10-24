147aab98f0d9bf5e4cfed1bbb8e43e8fe162a71d
Dani Lerner <dlerner97@gmail.com>
2021-10-24 16:38:24 -0400

    Added operators for instances of the detection struct. Needed for easier implementation of a few tests.

---
+++ app/Detection.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+
+#include "../include/Detection.hpp"
+
+Detection& Detection::operator=(const Detection& detect) {
+    return *this = Detection(detect);
+}
+
+Detection Detection::operator+(const Detection& detect) const {
+    Detection res;
+    res.x = this->x + detect.x;
+    res.y = this->y + detect.y;
+    res.width = this->width + detect.width;
+    res.height = this->height + detect.height;
+    return res;
+}
+
+Detection Detection::operator-(const Detection& detect) const {
+    Detection res;
+    res.x = std::abs(this->x - detect.x);
+    res.y = std::abs(this->y - detect.y);
+    res.width = std::abs(this->width - detect.width);
+    res.height = std::abs(this->height - detect.height);
+    return res;
+}
+
+Detection& Detection::operator+=(const Detection& detect) {
+    this->x += detect.x;
+    this->y += detect.y;
+    this->width += detect.width;
+    this->height += detect.height;
+    return *this;
+}
--- include/Detection.hpp
+++ include/Detection.hpp
@@ -4,10 +4,23 @@
  * @brief This struct will be used to pass detection information between the human detector and position estimator classes. See the UML for more information. 
  * 
  */
-struct Detection
-{
+struct Detection {
     const int NO_HUMAN_DETECTED = -1;
-    double probabilty{};
-    int x{}, y{};
-    int width{}, height{};
+    double probabilty{0};
+    int x{0}, y{0};
+    int width{0}, height{0};
+    Detection& operator=(const Detection& detect);
+    Detection operator+(const Detection& detect) const;
+    Detection operator-(const Detection& detect) const;
+    Detection& operator+=(const Detection& detect);
+
+    Detection() {}
+
+    Detection(const Detection& detect) {
+        this->probabilty = detect.probabilty;
+        this->x = detect.x;
+        this->y = detect.y;
+        this->width = detect.width;
+        this->height = detect.height;
+    }
 };
