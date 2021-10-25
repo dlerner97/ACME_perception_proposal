8b39cd0e2b5632222fb9cdcb361e7242c8f08654
Dani Lerner <dlerner97@gmail.com>
2021-10-24 23:14:32 -0400

    Fixed error in VisionAPI class

--- include/VisionAPI.hpp
+++ include/VisionAPI.hpp
@@ -13,7 +13,7 @@
 class VisionAPI {
   private:
     const std::unordered_map<std::string, double>& robot_params;
-//    HumanDetector detector;
+    // HumanDetector detector;
     PositionEstimator estimator;
     std::array<double, 2> alert_thresholds{};
 
@@ -26,13 +26,11 @@ class VisionAPI {
         alert_thresholds[1] = robot_params.at("HIGH_ALERT_THRESHOLD");
       }
 
-    // ~VisionAPI();
-
     /**
      * @brief This method takes an image, finds all people within the image, and outputs the estimated postions in the ROBOT frame.
      * 
      * @param img
      * @return All estimated x, y, z positions of people in a given image.
      */
-    std::vector<std::array<double, 3>> get_xyz(cv::Mat);
+    std::vector<std::array<double, 3> > get_xyz(const cv::Mat&);
 };
